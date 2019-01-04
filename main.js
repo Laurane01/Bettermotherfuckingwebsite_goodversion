9e3a49e282b8461c65f1f7e920eaea5d76c1c0ae
Lau Petit <petitlaurane01@gmail.com>
2019-01-04 19:29:11 +0100

    Ajout du popup

---
+++ main.js
@@ -0,0 +1,9 @@
+setTimeout(function() {alert("Bienvenue sur le site xxxvidsxxx !") }, 10000);
+var personn = prompt("Alors, est-ce que cela vous tente de faire un tour sur notre super site xxxvidsxxx? Repondez par Oui et vous atterrirez directement sur notre page. Sinon, faites la touche Entree.");
+if (personn != null) window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+setTimeout(function() {prompt("Alors, est-ce que cela vous tente de faire un tour sur notre super site xxxvidsxxx? Repondez par Oui et vous atterrirez directement sur notre page. Sinon, faites la touche Entree.") }, 13000);
+
+
+
+
+
